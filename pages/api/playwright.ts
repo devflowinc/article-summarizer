56496341da1bf8657928d3fb276bda2e6b502081
Nicholas Khami <nicholas.khami@gmail.com>
2023-02-08 00:02:03 -0600

    attempt with puppeteer-chromium-resolver

--- pages/api/playwright.ts
+++ pages/api/playwright.ts
@@ -1,12 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer";
+const PCR = require("puppeteer-chromium-resolver");
 
+const getPuppeteer = async () => {
+  const PCR = require("puppeteer-chromium-resolver");
+  const option = {
+    revision: "",
+    detectionPath: "",
+    folderName: ".chromium-browser-snapshots",
+    defaultHosts: [
+      "https://storage.googleapis.com",
+      "https://npm.taobao.org/mirrors",
+    ],
+    hosts: [],
+    cacheRevisions: 2,
+    retry: 3,
+    silent: false,
+  };
+  const stats = await PCR(option);
+  return stats;
+};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { url } = req;
-  const browser = await puppeteer.launch({
+  const stats = await getPuppeteer();
+  const browser = await stats.puppeteer.launch({
     headless: true, // setting this to true will not run the ui
+    args: ["--no-sandbox"],
+    executablePath: stats.executablePath,
   });
 
   if (!url) {
@@ -16,11 +40,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   let slug = url.split(".com")[1];
 
   try {
-
     const page = await browser.newPage();
     await page.goto(`https://terminal.hackernoon.com${slug}`);
 
-    await page.waitForSelector('pre');
+    await page.waitForSelector("pre");
 
     let text = await page.evaluate(() => {
       return document.querySelector("pre")?.innerText;
@@ -28,9 +51,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     await browser.close();
 
-    text = text && text
-      .replace(/(\r\n|\n|\r)/gm, "")
-      .replace(/(\r\t|\t|\r)/gm, "");
+    text =
+      text && text.replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
 
     res.status(200).json({ text });
   } catch (error) {
