e653bc7dfecaf543c2c1d1be460238a2ade62118
Nicholas Khami <nicholas.khami@gmail.com>
2023-02-08 00:48:42 -0600

    add image optimization package and favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,23 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.webp"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.webp"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.webp"
+          />
           <meta name="description" content={description} />
           <meta property="og:site_name" content={sitename} />
           <meta property="og:description" content={description} />
